961942bd82ec67d89ee06f849c08b6926e7c18aa
momchil-iliev <momchil.s.iliev@gmail.com>
2016-05-24 16:23:49 +0300

    Problem 8

--- Pr8.RecursiveFibonacci/Program.cs
+++ Pr8.RecursiveFibonacci/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,51 @@ namespace Pr8.RecursiveFibonacci
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(GetFibonachi(n));
+        }
+
+       private static Dictionary<int, long> fib = new Dictionary<int, long>(); 
+
+        private static long GetFibonachi(int n)
+        {
+            if (n ==0)
+            {
+                if (!fib.ContainsKey(0))
+                {
+                    fib.Add(0,0);
+                }
+                return fib[0];
+            }
+            else if(n==1)
+            {
+                if (!fib.ContainsKey(1))
+                {
+                    fib.Add(1,1);
+                }
+                return fib[1];
+            }
+            else if (n == 2)
+            {
+                if (!fib.ContainsKey(2))
+                {
+                    fib.Add(2, 1);
+                }
+                return fib[2];
+            }
+            else
+            {
+                if (fib.ContainsKey(n))
+                {
+                    return fib[n];
+                }
+                else
+                {
+                    fib.Add(n, GetFibonachi(n - 1) + GetFibonachi(n - 2));
+                    return fib[n];
+                }
+            }
         }
     }
 }
